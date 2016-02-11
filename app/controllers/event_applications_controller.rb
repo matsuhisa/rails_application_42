932e952a9f8c61949163a6e1392b06ece50eec08
matsuhisa <matsuhisa+github@gmail.com>
2016-02-11 22:38:59 +0900

    Use i18n for japanease

--- app/controllers/event_applications_controller.rb
+++ app/controllers/event_applications_controller.rb
@@ -2,7 +2,7 @@ class EventApplicationsController < ApplicationController
   permits :name, :name_ruby, :gender, :email, :phone, :notes
 
   def new
-    @event_application = EventApplication.new
+    @event_application = EventApplication.new(gender: EventApplication::FEMALE)
   end
 
   def confirm(event_application)
--- app/models/event_application.rb
+++ app/models/event_application.rb
@@ -1,3 +1,11 @@
 class EventApplication < ActiveRecord::Base
-  validates :name, presence: true, length: { maximum: 20 }
+  MALE    = 1
+  FEMALE  = 2
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :name_ruby, presence: true, length: { maximum: 30 }
+  validates :gender, presence: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, length: { maximum: 256 }
+  validates :phone, presence: true, length: { maximum: 30 }
+
 end
