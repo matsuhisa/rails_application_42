28b30b17ce61f814ab4aef6e153db54df689270f
matsuhisa <matsuhisa+github@gmail.com>
2016-02-11 22:15:09 +0900

    Add views/vent_applications

--- app/controllers/event_applications_controller.rb
+++ app/controllers/event_applications_controller.rb
@@ -1,7 +1,21 @@
 class EventApplicationsController < ApplicationController
+  permits :name, :name_ruby, :gender, :email, :phone, :notes
 
   def new
     @event_application = EventApplication.new
+  end
+
+  def confirm(event_application)
+    @event_application = EventApplication.new(event_application)
+    unless @event_application.valid? && params['new'].blank?
+      render(:new)
+    end
+  end
+
+  def create
+  end
+
+  def complete
   end
 
 end
--- app/models/event_application.rb
+++ app/models/event_application.rb
@@ -1,2 +1,3 @@
 class EventApplication < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 20 }
 end
